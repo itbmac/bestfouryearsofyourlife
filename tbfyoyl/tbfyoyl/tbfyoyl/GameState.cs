dd5558a3f3e1254787e3f69c03bd9b533351be18
matthewdu77 <matthewdu77@gmail.com>
2013-09-29 13:59:59 -0400

    Added basic object framework

---
+++ tbfyoyl/tbfyoyl/tbfyoyl/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tbfyoyl
+{
+    public static class GameState
+    {
+        public static int bloodSugar;
+        public static int money;
+
+    }
+}
---
+++ tbfyoyl/tbfyoyl/tbfyoyl/MainGame.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace tbfyoyl
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class MainGame : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        DrawableGameComponent[] games;
+        DrawableGameComponent activeGame;
+
+        public const int NUM_GAMES = 5;
+
+        public MainGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            games = new DrawableGameComponent[NUM_GAMES];
+            games[0] = new UI(this); //TODO: add enum for games[INDEX]
+            games[1] = new WorldMap(this);
+            games[2] = new BookstoreGame(this);
+            games[3] = new TAGame(this);
+            games[4] = new SplashScreen(this);
+            activeGame = games[4];
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic 
+            base.Initialize();
+            for(int i = 0; i < NUM_GAMES; i++)
+                games[i].Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            activeGame.Update(gameTime);
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            activeGame.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            activeGame.Draw(gameTime);
+
+            base.Draw(gameTime);
+        }
+    }
+}
