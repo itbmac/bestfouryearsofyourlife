66f63e27678d67c1e3746cc1e13af5f7db6198de
itbmac <itbmac@yahoo.com>
2013-11-03 20:40:41 -0500

    Added Spine Library to Game & Example Animation  to Bookstore Game

--- tbfyoyl/tbfyoyl/tbfyoyl/BookstoreGame/BookstoreGame.cs
+++ tbfyoyl/tbfyoyl/tbfyoyl/BookstoreGame/BookstoreGame.cs
@@ -1,6 +1,6 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-
+using Spine;
 
 namespace tbfyoyl
 {
@@ -17,6 +17,56 @@ namespace tbfyoyl
     /// </summary>
     public class BookstoreGame : Minigame
     {
+        SkeletonRenderer skeletonRenderer;
+		Skeleton skeleton;
+		Slot headSlot;
+		AnimationState state;
+		SkeletonBounds bounds = new SkeletonBounds();
+        int speed = 1;
+
+		protected override void LoadContent () {
+			skeletonRenderer = new SkeletonRenderer(GraphicsDevice);
+			skeletonRenderer.PremultipliedAlpha = true;
+
+			String name = "spineboy"; // "goblins";
+
+			Atlas atlas = new Atlas("data/" + name + ".atlas", new XnaTextureLoader(GraphicsDevice));
+			SkeletonJson json = new SkeletonJson(atlas);
+			skeleton = new Skeleton(json.ReadSkeletonData("data/" + name + ".json"));
+			if (name == "goblins") skeleton.SetSkin("goblingirl");
+			skeleton.SetSlotsToSetupPose(); // Without this the skin attachments won't be attached. See SetSkin.
+
+			// Define mixing between animations.
+			AnimationStateData stateData = new AnimationStateData(skeleton.Data);
+			if (name == "spineboy") {
+				stateData.SetMix("walk", "jump", 0.2f);
+				stateData.SetMix("jump", "walk", 0.4f);
+			}
+
+			state = new AnimationState(stateData);
+
+			if (false) {
+				// Event handling for all animations.
+				state.Start += Start;
+				state.End += End;
+				state.Complete += Complete;
+				state.Event += Event;
+
+				state.SetAnimation(0, "drawOrder", true);
+			} else {
+				state.SetAnimation(0, "walk", false);
+				TrackEntry entry = state.AddAnimation(0, "jump", false, 0);
+				entry.End += new EventHandler<StartEndArgs>(End); // Event handling for queued animations.
+				state.AddAnimation(0, "walk", true, 0);
+			}
+
+			skeleton.X = 320;
+			skeleton.Y = 440;
+			skeleton.UpdateWorldTransform();
+
+			headSlot = skeleton.FindSlot("head");
+		}
+        
         public BookstoreGame(MainGame game)
             : base(game)
         {
@@ -41,12 +91,60 @@ namespace tbfyoyl
         public override void Update(GameTime gameTime)
         {
             // TODO: Add your update code here
+            state.Update(gameTime.ElapsedGameTime.Milliseconds / 1000f);
+            state.Apply(skeleton);
 
             base.Update(gameTime);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
+            if (skeleton.X > 640 || skeleton.X < 0)
+            {
+                speed = -1 * speed;
+                skeleton.FlipX = !skeleton.FlipX;
+            }
+
+            skeleton.X += speed;
+
+            skeleton.UpdateWorldTransform();
+            skeletonRenderer.Begin();
+            skeletonRenderer.Draw(skeleton);
+            skeletonRenderer.End();
+
+            bounds.Update(skeleton, true);
+            MouseState mouse = Mouse.GetState();
+            headSlot.G = 1;
+            headSlot.B = 1;
+            if (bounds.AabbContainsPoint(mouse.X, mouse.Y))
+            {
+                BoundingBoxAttachment hit = bounds.ContainsPoint(mouse.X, mouse.Y);
+                if (hit != null)
+                {
+                    headSlot.G = 0;
+                    headSlot.B = 0;
+                }
+            }
+        }
+
+        public void Start(object sender, StartEndArgs e)
+        {
+            Console.WriteLine(e.TrackIndex + " " + state.GetCurrent(e.TrackIndex) + ": start");
+        }
+
+        public void End(object sender, StartEndArgs e)
+        {
+            Console.WriteLine(e.TrackIndex + " " + state.GetCurrent(e.TrackIndex) + ": end");
+        }
+
+        public void Complete(object sender, CompleteArgs e)
+        {
+            Console.WriteLine(e.TrackIndex + " " + state.GetCurrent(e.TrackIndex) + ": complete " + e.LoopCount);
+        }
+
+        public void Event(object sender, EventTriggeredArgs e)
+        {
+            Console.WriteLine(e.TrackIndex + " " + state.GetCurrent(e.TrackIndex) + ": event " + e.Event);
         }
 
     }
